20b0c5f846496f4373f4b958080af2c8163af0ae
Elena Moreva <lenka.moreva@gmail.com>
2024-09-24 12:58:15 +0300

    project name changed

--- drones/src/main/java/com/transport/drones/DronesApplication.java
+++ drones/src/main/java/com/transport/drones/DronesApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.transport.drones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DronesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DronesApplication.class, args);
 	}
 
 }
--- drones/src/test/java/com/transport/drones/DronesApplicationTests.java
+++ drones/src/test/java/com/transport/drones/DronesApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.transport.drones;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DronesApplicationTests {
 
 	@Test
 	void contextLoads() {
