ade65bd26c459869e675f98b8ff3480be3ad56b1
Hippo Maru <hippomaru@ya.ru>
2024-12-16 00:33:08 +0300

    lab2 done

--- drones/src/main/java/com/transport/drones/DronesApplication.java
+++ drones/src/main/java/com/transport/drones/DronesApplication.java
@@ -1,7 +1,13 @@
 package com.transport.drones;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.boot.actuate.info.MapInfoContributor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class DronesApplication {
@@ -9,5 +15,13 @@ public class DronesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DronesApplication.class, args);
 	}
-
+	@Bean
+	InfoContributor getInfoContributor() {
+		Map<String, Object> details = new HashMap<>();
+		details.put("nameApp", "Drones");
+		details.put("developer", "Elena Moreva");
+		Map<String, Object> wrapper = new HashMap<>();
+		wrapper.put("info", details);
+		return new MapInfoContributor(wrapper);
+	}
 }
